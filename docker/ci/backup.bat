2fbb496c8c0bf47f629764ad30cda0a4f5d8c38f
JByfordRew <jeremybyfordrew@gmail.com>
2019-04-13 11:05:02 +0100

    Create backup.bat

---
+++ docker/ci/backup.bat
@@ -0,0 +1,9 @@
+f3
+@echo off
+docker-compose down
+call ./volumes/backup.bat gogsdata
+call ./volumes/backup.bat teamcitydata
+call ./volumes/backup.bat teamcitylogs
+call ./volumes/backup.bat sqldata
+call ./volumes/backup.bat mediawikiimages
+call ./volumes/backup.bat mediawikidata
